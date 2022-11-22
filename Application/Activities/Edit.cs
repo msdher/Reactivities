fdd0db7913b06445341bb43b0865b93c7adc9039
Saleedher M <saleedher.m@stryker.com>
2022-11-22 23:03:54 +0530

    End of section 4

---
+++ Application/Activities/Edit.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Domain;
+using MediatR;
+using Persistence;
+
+namespace Application.Activities
+{
+    public class Edit
+    {
+        public class Command : IRequest
+        {
+            public Activity Activity { get; set; }
+        }
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly DataContext _context;
+            public readonly IMapper _mapper;
+
+            public Handler(DataContext dataContext, IMapper mapper)
+            {
+                _mapper = mapper;
+                _context = dataContext;
+            }
+
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var activity = await _context.Activities.FindAsync(request.Activity.Id);
+                _mapper.Map(request.Activity, activity);
+                await _context.SaveChangesAsync();
+                return Unit.Value;
+
+            }
+        }
+    }
+}
